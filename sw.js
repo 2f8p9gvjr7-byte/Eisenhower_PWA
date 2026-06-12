ca27371c7cadd512c472bb0f6c779bc77ae64230
2f8p9gvjr7-byte <2f8p9gvjr7@privaterelay.appleid.com>
2026-06-12 09:36:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const CACHE = 'eisenhower-v10';
+const CACHE = 'eisenhower-pro-v1';
 const ASSETS = [
-  '/Eisenhower_PWA/',
-  '/Eisenhower_PWA/index.html',
-  '/Eisenhower_PWA/manifest.json',
-  '/Eisenhower_PWA/icon-192.png',
-  '/Eisenhower_PWA/icon-512.png',
+  '/Eisenhower_Pro/',
+  '/Eisenhower_Pro/index.html',
+  '/Eisenhower_Pro/manifest.json',
+  '/Eisenhower_Pro/icon-192.png',
+  '/Eisenhower_Pro/icon-512.png',
   'https://cdn.jsdelivr.net/npm/sortablejs@1.15.2/Sortable.min.js'
 ];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
@@ -25,7 +26,7 @@ self.addEventListener('fetch', e => {
         const clone = resp.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
         return resp;
-      }).catch(() => caches.match('/Eisenhower_PWA/index.html'));
+      }).catch(() => caches.match('/Eisenhower_Pro/index.html'));
     })
   );
 });
